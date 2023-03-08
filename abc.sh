fbf10d64120136f28ae5ad75bb54910235ed4323
jbstyle20 <121085600+jbstyle20@users.noreply.github.com>
2023-03-08 10:04:24 +0800

    Create abc.sh

---
+++ abc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Clients:"
+awk '/^###/ {print NR".",$2,$3,$4}' "/root/ssh.txt"
+
+
+read -p "Enter the number of the client to delete: " CLIENT_NUMBER
+
+awk -v n="${CLIENT_NUMBER}" '/^###/ && NR==n {next} 1' "/root/ssh.txt" > "/root/ssh.txt.tmp"
+mv "/root/ssh.txt.tmp" "/root/ssh.txt"
+
+echo "Client deleted."
